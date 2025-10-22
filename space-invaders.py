336e170f4989287a83c32caa5ecdd923bbbacc61
Leonid <bohdanovleonid93@gmail.com>
2025-10-22 13:02:15 +0300

    add enemy update def setup()

--- space-invaders.py
+++ space-invaders.py
@@ -11,16 +11,29 @@ class MyGame(arcade.Window):
 
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-        self.background = arcade.load_texture("background_space.png")
         self.player_list = None
+        self.player_sprite = None
+        self.enemy_list = None
+        self.enemy_sprite = None
+        self.background = arcade.load_texture("background_space.png")
 
     def setup(self):
+        # Встановлюємо гравця
+        self.player_list = arcade.SpriteList()
         self.player_sprite = arcade.Sprite(
             ":resources:images/space_shooter/playerShip1_orange.png", 0.5
         )
         self.player_sprite.center_x = WINDOW_WIDTH / 2
         self.player_sprite.bottom = 10
-        self.player_sprite.append(self.player_list)
+        self.player_list.append(self.player_sprite)
+        # Встановлюємо ворога
+        self.enemy_list = arcade.SpriteList()
+        self.enemy_sprite = arcade.Sprite(
+            ":resources:images/space_shooter/playerShip1_green.png", 0.5
+        )
+        self.enemy_sprite.center_x = WINDOW_WIDTH / 2
+        self.enemy_sprite.bottom = 10
+        self.enemy_list.append(self.enemy_sprite)
 
     def on_draw(self):
         self.clear()
@@ -28,6 +41,7 @@ class MyGame(arcade.Window):
             self.background,
             arcade.LBWH(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
         )
+        self.player_list.draw()
 
 
 def main():
