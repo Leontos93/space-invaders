d1de4dd386bd0ada0e3695ce0eb06010182bcf16
Leonid <bohdanovleonid93@gmail.com>
2025-10-22 12:25:59 +0300

    add sprite of player

--- space-invaders.py
+++ space-invaders.py
@@ -4,6 +4,7 @@ import arcade
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 700
 WINDOW_TITLE = "Space invaders"
+PLAYER_MOVEMENT_SPEED = 2
 
 
 class MyGame(arcade.Window):
@@ -11,9 +12,15 @@ class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
         self.background = arcade.load_texture("background_space.png")
+        self.player_list = None
 
     def setup(self):
-        pass
+        self.player_sprite = arcade.Sprite(
+            ":resources:images/space_shooter/playerShip1_orange.png", 0.5
+        )
+        self.player_sprite.center_x = WINDOW_WIDTH / 2
+        self.player_sprite.bottom = 10
+        self.player_sprite.append(self.player_list)
 
     def on_draw(self):
         self.clear()
