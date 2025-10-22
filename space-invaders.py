e5a9eaeb2d651f4e60ed057dbcc690a521ab6165
Leonid <bohdanovleonid93@gmail.com>
2025-10-22 11:53:54 +0300

    add main Window in arcade

---
+++ space-invaders.py
@@ -0,0 +1,29 @@
+import arcade
+
+# Constants
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Space invaders"
+
+
+class GameView(arcade.Window):
+
+    def __init__(self):
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        self.clear()
+
+
+def main():
+    window = GameView()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
