94d26b1ed278959bb19a855dc6a558e43809f255
Leonid <bohdanovleonid93@gmail.com>
2025-10-22 12:12:39 +0300

    add background on window and update def on_draw()

--- space-invaders.py
+++ space-invaders.py
@@ -1,8 +1,8 @@
 import arcade
 
 # Constants
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 700
 WINDOW_TITLE = "Space invaders"
 
 
@@ -10,13 +10,17 @@ class MyGame(arcade.Window):
 
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+        self.background = arcade.load_texture("background_space.png")
 
     def setup(self):
         pass
 
     def on_draw(self):
         self.clear()
+        arcade.draw_texture_rect(
+            self.background,
+            arcade.LBWH(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
+        )
 
 
 def main():
